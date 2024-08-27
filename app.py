34cedc5b9873925068ee8c7f3921e0d62a2ae190
msangalli1 <msangalli94@gmail.com>
2024-08-26 22:05:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
 from flask import Flask, request, jsonify
 from gtts import gTTS
 import os
-from flask import send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/text-to-speech', methods=['POST'])
 def text_to_speech():
-    data = request.json
+    data = request.get_json()
     text = data.get('text', '')
-    if not text:
-        return jsonify({'message': 'No text provided'}), 400
-
-    tts = gTTS(text)
-    audio_file = 'audio.mp3'
-    tts.save(audio_file)
-
-    return jsonify({'message': 'Audio file created', 'audio_url': f'/audio/{audio_file}'})
-
-@app.route('/audio/<filename>')
-def download_file(filename):
-    return send_from_directory('.', filename, as_attachment=True)
+    if text:
+        tts = gTTS(text=text, lang='en')
+        file_path = '/app/audio.mp3'
+        tts.save(file_path)
+        return jsonify({'message': 'Audio file created successfully', 'audio_url': f'/audio.mp3'})
+    return jsonify({'error': 'No text provided'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)  # Configura para escuchar en todas las interfaces y el puerto 5000
